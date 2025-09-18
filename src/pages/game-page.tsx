442eaab37587a67cec480db1e323777219cd1893
Roman-PRV <prokopenkorv@gmail.com>
2025-09-18 22:03:15 +0200

    feat: create pages templates ss-004

---
+++ src/pages/game-page.tsx
@@ -0,0 +1,5 @@
+const GamePage: React.FC = () => {
+	return <h1>Game Page</h1>;
+};
+
+export { GamePage };
---
+++ src/pages/game-selection-page.tsx
@@ -0,0 +1,5 @@
+const GameSelectionPage: React.FC = () => {
+	return <h1>GameSelectionPage</h1>;
+};
+
+export { GameSelectionPage };
