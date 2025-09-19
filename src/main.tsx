2f93fa252e50f80614214c40f5e783abd3aa2314
Roman-PRV <prokopenkorv@gmail.com>
2025-09-19 08:17:42 +0200

    refactor: remove default app content ss-004

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+
 import { App } from "./app.js";
+import { router } from "./libs/components/router-provider/router-provider";
 
 const root = document.querySelector("#root");
 
@@ -12,6 +15,7 @@ if (!root) {
 
 createRoot(root).render(
 	<StrictMode>
+		<RouterProvider router={router} />
 		<App />
 	</StrictMode>
 );
