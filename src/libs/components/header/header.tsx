ab339af4b42037677b6900b36b207109e35c7afc
Roman-PRV <prokopenkorv@gmail.com>
2025-09-22 19:49:52 +0200

    feat: create helper getValidClassNames ss-005

--- src/libs/components/header/header.tsx
+++ src/libs/components/header/header.tsx
@@ -1,8 +1,17 @@
 import { Logo, Navigation } from "~/libs/components/components";
+import { getValidClassNames } from "~/libs/helpers/get-valid-class-names.helper";
+
+import styles from "./styles.module.css";
 
 const Header: React.FC = () => {
 	return (
-		<header className="flex bg-red-50">
+		<header
+			className={getValidClassNames(
+				styles["header"],
+				"flex items-center justify-between px-6 py-4"
+			)}
+		>
+
 			<Logo />
 			<Navigation />
 		</header>
