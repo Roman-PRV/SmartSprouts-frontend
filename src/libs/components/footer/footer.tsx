3ea2e42f016e8efa3b606185456d34a4fc76d3ba
Roman-PRV <prokopenkorv@gmail.com>
2025-09-23 18:55:16 +0200

    feat: create footer ss-005

--- src/libs/components/footer/footer.tsx
+++ src/libs/components/footer/footer.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 
+import { getValidClassNames } from "~/libs/helpers/helpers";
+
+import styles from "./styles.module.css";
+
 const Footer: React.FC = () => {
 	return (
-		<footer className="bg-red-50">
+		<footer className={getValidClassNames(styles["footer"])}>
 			<p>© 2025 SmartSprouts. All rights reserved.</p>
 		</footer>
 	);
