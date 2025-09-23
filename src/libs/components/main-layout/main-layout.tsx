2c1078244fbc9ef65dff09049c4a536340030beb
Roman-PRV <prokopenkorv@gmail.com>
2025-09-23 18:04:44 +0200

    refactor: change layout__main ss-005

--- src/libs/components/main-layout/main-layout.tsx
+++ src/libs/components/main-layout/main-layout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 
 import { Footer, Header } from "~/libs/components/components";
+import { getValidClassNames } from "~/libs/helpers/helpers";
 
 import styles from "./styles.module.css";
 
 const MainLayout: React.FC = () => {
 	return (
-		<div className={styles["layout__container"]}>
+		<div className={getValidClassNames(styles["layout__container"], "flex flex-col")}>
 			<Header />
-			<main className="flex-1 p-4 sm:p-6 lg:p-8">
+			<main className={getValidClassNames(styles["layout__main"], "flex-1 p-4 sm:p-6 lg:p-8")}>
 				<Outlet />
 			</main>
 
