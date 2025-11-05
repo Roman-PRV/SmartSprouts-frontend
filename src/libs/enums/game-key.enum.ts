ea8da148022f596f1b0a69aac25f73508b21172c
Roman-PRV <prokopenkorv@gmail.com>
2025-11-05 17:03:01 +0100

    fix: match the gamekey with what the backend returns ss-011

--- src/libs/enums/game-key.enum.ts
+++ src/libs/enums/game-key.enum.ts
@@ -1,7 +1,7 @@
 const GameKey = {
-	FIND_THE_WRONG: "FindTheWrong",
-	TRUE_FALSE_IMAGE: "TrueFalseImage",
-	TRUE_FALSE_TEXT: "TrueFalseText",
+	FIND_THE_WRONG: "find_the_wrong",
+	TRUE_FALSE_IMAGE: "true_false_image",
+	TRUE_FALSE_TEXT: "true_false_text",
 } as const;
 
 export { GameKey };
