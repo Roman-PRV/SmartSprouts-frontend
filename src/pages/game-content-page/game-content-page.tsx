21e9b86cd33a3a36f32d0741456e42c42ccfd7a9
Roman-PRV <prokopenkorv@gmail.com>
2025-11-05 17:25:16 +0100

    feat: implement the selection of the gamepreview component depending on the game ss-011

--- src/pages/game-content-page/game-content-page.tsx
+++ src/pages/game-content-page/game-content-page.tsx
@@ -1,9 +1,34 @@
 import { useParams } from "react-router-dom";
 
+import { type GameKey } from "~/libs/enums/enums";
+import { getValidClassNames } from "~/libs/helpers/helpers";
+import { useAppSelector } from "~/libs/hooks/hooks";
+
+import { GamePreviewComponentMap } from "./game-preview-component-map";
+import styles from "./styles.module.css";
+
 const GameContentPage: React.FC = () => {
 	const { id } = useParams();
+	type GameKeyType = (typeof GameKey)[keyof typeof GameKey];
+	const currentGame = useAppSelector((state) => state.games.currentGame);
 
-	return <h1>Game Content Page: {id}</h1>;
+	if (!currentGame) {
+		return (
+			<div className={getValidClassNames(styles["loading-container"])}>
+				<h1>Loading game content...</h1>
+			</div>
+		);
+	}
+
+	const gameKey = currentGame.key as GameKeyType;
+	const GamePreviewComponent = GamePreviewComponentMap[gameKey];
+
+	return (
+		<h1>
+			Game Content Page: {id}
+			<GamePreviewComponent game={currentGame} />
+		</h1>
+	);
 };
 
 export { GameContentPage };
--- src/pages/game-selection-page/game-card.tsx
+++ src/pages/game-selection-page/game-card.tsx
@@ -1,6 +1,8 @@
 import { Link } from "~/libs/components/components";
 import { getValidClassNames } from "~/libs/helpers/helpers";
+import { useAppDispatch, useCallback } from "~/libs/hooks/hooks";
 import { type GameDescriptionDto } from "~/libs/types/types";
+import { actions as gamesActions } from "~/modules/games/games";
 
 import styles from "./styles.module.css";
 
@@ -9,8 +11,18 @@ type Properties = {
 };
 
 const GameCard: React.FC<Properties> = ({ game }) => {
+	const dispatch = useAppDispatch();
+
+	const handleCardClick = useCallback(() => {
+		dispatch(gamesActions.setCurrentGame(game));
+	}, [game, dispatch]);
+
 	return (
-		<Link className={getValidClassNames(styles["card"])} to={`/games/${game.id}`}>
+		<Link
+			className={getValidClassNames(styles["card"])}
+			onClick={handleCardClick}
+			to={`/games/${game.id}`}
+		>
 			<img alt={game.title} className={getValidClassNames(styles["image"])} src={game.icon_url} />
 			<div className={getValidClassNames(styles["content"])}>
 				<h2 className={getValidClassNames(styles["cardTitle"])}>{game.title}</h2>
