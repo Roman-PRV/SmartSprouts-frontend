8dae26a20a0685276a2a5d10ad9737c05bc7c66a
Roman-PRV <prokopenkorv@gmail.com>
2025-11-05 12:03:56 +0100

    feat: create GameKey enum ss-011

--- src/libs/enums/enums.ts
+++ src/libs/enums/enums.ts
@@ -2,6 +2,7 @@ export { APIPath } from "./api-path.enum";
 export { AppEnvironment } from "./app-environment.enum";
 export { ContentType } from "./content-type.enum";
 export { DataStatus } from "./data-status.enum";
+export { GameKey } from "./game-key.enum";
 export { LogoSize } from "./logo-size.enum";
 export { NotificationMessage } from "./notification-message.enum";
 export { ServerErrorType } from "./server-error-type.enum";
---
+++ src/libs/enums/game-key.enum.ts
@@ -0,0 +1,7 @@
+const GameKey = {
+	FIND_THE_WRONG: "FindTheWrong",
+	TRUE_FALSE_IMAGE: "TrueFalseImage",
+	TRUE_FALSE_TEXT: "TrueFalseText",
+} as const;
+
+export { GameKey };
