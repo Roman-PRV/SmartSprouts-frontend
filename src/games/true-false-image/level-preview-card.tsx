985530aefd4f7c6f93b45e7ac2bd9437c02fb76a
Roman-PRV <prokopenkorv@gmail.com>
2025-11-12 20:31:50 +0100

    feat: implement levels preview for TrueFalseImage game ss-011

---
+++ src/games/true-false-image/level-preview-card.tsx
@@ -0,0 +1,32 @@
+import { Link } from "~/libs/components/components";
+import { getValidClassNames } from "~/libs/helpers/helpers";
+import { type GameDescriptionDto, type LevelDescriptionDto } from "~/libs/types/types";
+
+import styles from "./styles.module.css";
+
+type Properties = {
+	game: GameDescriptionDto;
+	level: LevelDescriptionDto;
+	number: number;
+};
+
+const LevelPreviewCard: React.FC<Properties> = ({ game, level, number }) => {
+	return (
+		<Link
+			className={getValidClassNames(styles["card"])}
+			to={`/games/${game.id}/levels/${level.id}`}
+		>
+			<img
+				alt={level.title}
+				className={getValidClassNames(styles["card__image"])}
+				src={level.image_url}
+			/>
+			<div className={getValidClassNames(styles["card__content"])}>
+				<p className={getValidClassNames(styles["card__number"])}>Level {number++}</p>
+				<p className={getValidClassNames(styles["card__title"])}>{level.title}</p>
+			</div>
+		</Link>
+	);
+};
+
+export { LevelPreviewCard };
--- src/games/true-false-image/true-false-image-preview.tsx
+++ src/games/true-false-image/true-false-image-preview.tsx
@@ -1,5 +1,50 @@
+import { useParams } from "react-router-dom";
+
+import { EMPTY_ARRAY_LENGTH } from "~/libs/constants/constants";
+import { getValidClassNames } from "~/libs/helpers/helpers";
+import { useAppDispatch, useAppSelector, useEffect } from "~/libs/hooks/hooks";
+import { actions as gamesActions } from "~/modules/games/games";
+
+import { LevelPreviewCard } from "./level-preview-card";
+import styles from "./styles.module.css";
+
 const TrueFalseImagePreview: React.FC = () => {
-	return <div>TrueFalseImagePreview</div>;
+	const { id: gameId } = useParams();
+	const dispatch = useAppDispatch();
+
+	const currentGame = useAppSelector((state) => state.games.currentGame);
+	const currentGameLevels = useAppSelector((state) => state.games.currentGameLevels);
+
+	useEffect(() => {
+		if (!currentGame && gameId) {
+			void dispatch(gamesActions.getById(gameId));
+		}
+	}, [dispatch, currentGame, gameId]);
+
+	useEffect(() => {
+		if (currentGame && !currentGameLevels) {
+			void dispatch(gamesActions.getLevelsList(currentGame.id));
+		}
+	}, [dispatch, currentGame, currentGameLevels]);
+
+	return (
+		<div>
+			<h2 className={getValidClassNames(styles["game-title"])}>
+				Select a level for the TrueFalseImage game
+			</h2>
+			<main aria-live="polite" className={getValidClassNames(styles["grid"])}>
+				{currentGameLevels && currentGameLevels.length > EMPTY_ARRAY_LENGTH && currentGame ? (
+					currentGameLevels.map((level, index) => (
+						<LevelPreviewCard game={currentGame} key={level.id} level={level} number={index} />
+					))
+				) : (
+					<div className={getValidClassNames(styles["no-content"])}>
+						No levels available at the moment.
+					</div>
+				)}
+			</main>
+		</div>
+	);
 };
 
 export { TrueFalseImagePreview };
