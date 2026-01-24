3a8c4b2f2c0ff7f1caeeb3923b0f2abf50db2042
Roman-PRV <prokopenkorv@gmail.com>
2026-01-24 22:54:59 +0100

    test: remove unnecessary import ss-044

--- tests/libs/components/dropdown/dropdown.spec.tsx
+++ tests/libs/components/dropdown/dropdown.spec.tsx
@@ -3,7 +3,7 @@
  */
 import { cleanup, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { afterEach, describe, expect, it, vi } from "vitest";
 
 import "@testing-library/jest-dom/vitest";
 
