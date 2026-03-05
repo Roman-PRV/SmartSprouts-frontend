f4e2f4c498e8c2cc5122aea7a93b6ff05acaaf16
Roman-PRV <prokopenkorv@gmail.com>
2026-03-05 22:22:02 +0100

    refactor: create GameLevelsContent component ss-049

--- src/libs/components/game-levels-preview/game-levels-preview.tsx
+++ src/libs/components/game-levels-preview/game-levels-preview.tsx
@@ -1,4 +1,3 @@
-import { LevelPreviewCard } from "~/libs/components/components";
 import { EMPTY_ARRAY_LENGTH } from "~/libs/constants/constants";
 import { DataStatus } from "~/libs/enums/enums";
 import { getValidClassNames } from "~/libs/helpers/helpers";
@@ -13,6 +12,7 @@ import {
 import { type GameDescriptionDto } from "~/libs/types/types";
 import { getLevelsList } from "~/modules/games/slices/actions";
 
+import { GameLevelsContent } from "./game-levels-content/game-levels-content";
 import styles from "./styles.module.css";
 
 type Properties = {
@@ -38,50 +38,22 @@ const GameLevelsPreview: React.FC<Properties> = ({ game }) => {
 
 	const isLoading = levelsStatus === DataStatus.PENDING;
 	const hasError = levelsStatus === DataStatus.REJECTED;
-	const hasLevels = currentGameLevels && currentGameLevels.length > EMPTY_ARRAY_LENGTH;
-
-	const renderContent = (): React.JSX.Element => {
-		if (isLoading) {
-			return (
-				<div className={getValidClassNames(styles["game-levels-preview__no-content"])}>
-					{t("games.levels.loading")}
-				</div>
-			);
-		}
-
-		if (hasError) {
-			return (
-				<div className={getValidClassNames(styles["game-levels-preview__no-content"])}>
-					{t("games.levels.error")}
-				</div>
-			);
-		}
-
-		if (!hasLevels) {
-			return (
-				<div className={getValidClassNames(styles["game-levels-preview__no-content"])}>
-					{t("games.levels.empty")}
-				</div>
-			);
-		}
-
-		return (
-			<>
-				{currentGameLevels.map((level, index) => (
-					<LevelPreviewCard game={game} key={level.id} level={level} number={index} />
-				))}
-			</>
-		);
-	};
+	const hasLevels = Boolean(currentGameLevels && currentGameLevels.length > EMPTY_ARRAY_LENGTH);
 
 	return (
 		<div>
 			<h2 className={getValidClassNames(styles["game-levels-preview__title"])}>
 				{t("games.levels.title", { title: game.title })}
 			</h2>
-			<main aria-live="polite" className={getValidClassNames(styles["game-levels-preview__grid"])}>
-				{renderContent()}
-			</main>
+			<section aria-live="polite" className={getValidClassNames(styles["game-levels-preview__grid"])}>
+				<GameLevelsContent
+					game={game}
+					hasError={hasError}
+					hasLevels={hasLevels}
+					isLoading={isLoading}
+					levels={currentGameLevels ?? []}
+				/>
+			</section>
 		</div>
 	);
 };
