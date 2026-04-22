15b49fde715b63b7ef36d20a9695918ad17cd56f
Roman-PRV <prokopenkorv@gmail.com>
2026-04-22 23:28:35 +0200

    feat: add localization to profile ss-061

---
+++ src/libs/modules/localization/locales/en/profile.ts
@@ -0,0 +1,8 @@
+export const profile = {
+	accuracy: "Accuracy",
+	completedLevels: "Completed Levels",
+	error: "Failed to load profile",
+	title: "My Profile",
+	totalLevels: "Total Levels",
+	totalScore: "Total Score",
+};
---
+++ src/libs/modules/localization/locales/uk/profile.ts
@@ -0,0 +1,8 @@
+export const profile = {
+	accuracy: "Точність",
+	completedLevels: "Завершено рівнів",
+	error: "Не вдалося завантажити профіль",
+	title: "Мій профіль",
+	totalLevels: "Всього рівнів",
+	totalScore: "Загальний рахунок",
+};
