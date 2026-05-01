621c36bb659f8cb9689ab693afe1d9e86ee8db6f
Roman-PRV <prokopenkorv@gmail.com>
2026-05-01 16:09:52 +0200

    refactor: simplify useAuthFormSubmit and remove redundant logic ss-069

--- src/modules/auth/hooks/use-auth-form-submit/use-auth-form-submit.hook.ts
+++ src/modules/auth/hooks/use-auth-form-submit/use-auth-form-submit.hook.ts
@@ -1,10 +1,8 @@
 import { type AsyncThunkAction } from "@reduxjs/toolkit";
-import { useCallback } from "react";
 import { type FieldValues, type UseFormSetError } from "react-hook-form";
 
-import { useAppDispatch, useFormSubmit } from "~/libs/hooks/hooks";
+import { useFormSubmit } from "~/libs/hooks/hooks";
 import { type AsyncThunkConfig } from "~/libs/types/types";
-import { actions as authActions } from "~/modules/auth/auth";
 
 type Properties<T extends FieldValues, R> = {
 	action: (argument: T) => AsyncThunkAction<R, T, AsyncThunkConfig>;
@@ -17,16 +15,7 @@ const useAuthFormSubmit = <T extends FieldValues, R>({
 	onSuccess,
 	setError,
 }: Properties<T, R>): ((payload: T) => Promise<void>) => {
-	const dispatch = useAppDispatch();
-	const handleFormSubmit = useFormSubmit({ action, onSuccess, setError });
-
-	return useCallback(
-		async (payload: T): Promise<void> => {
-			dispatch(authActions.clearError());
-			await handleFormSubmit(payload);
-		},
-		[dispatch, handleFormSubmit]
-	);
+	return useFormSubmit({ action, onSuccess, setError });
 };
 
 export { useAuthFormSubmit };
