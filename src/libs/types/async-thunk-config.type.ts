2b9add4afe473e16999f2296f5881f51af73ee6b
Roman-PRV <prokopenkorv@gmail.com>
2026-05-06 23:41:25 +0200

    fix: prevent duplicate error types ss-060

--- src/libs/types/async-thunk-config.type.ts
+++ src/libs/types/async-thunk-config.type.ts
@@ -1,13 +1,10 @@
 import { type store } from "~/libs/modules/store/store";
+import { type ThunkErrorPayload } from "~/libs/types/thunk-error.payload.type";
 
 type AsyncThunkConfig = {
 	dispatch: typeof store.instance.dispatch;
 	extra: typeof store.extraArguments;
-	rejectValue: {
-		errors?: Record<string, string[]> | undefined;
-		message: string;
-		status?: number | undefined;
-	};
+	rejectValue: ThunkErrorPayload;
 	state: ReturnType<typeof store.instance.getState>;
 };
 
