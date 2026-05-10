f9c2e4118f713e70fcb67d57b2998a938967320b
Roman-PRV <prokopenkorv@gmail.com>
2026-05-10 12:31:27 +0200

    refactor: remove temporary tests ss-074

--- test-eslint.ts
+++
@@ -1,9 +0,0 @@
-import { expect, vi } from 'vitest';
-const mockCheckAnswers = vi.fn();
-expect(mockCheckAnswers).toHaveBeenCalledWith(
-    expect.objectContaining({
-        payload: expect.objectContaining({
-            answers: expect.arrayContaining([]) as unknown
-        }) as unknown
-    })
-);
