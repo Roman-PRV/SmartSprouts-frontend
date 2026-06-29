1625650cd37c5351b38df443fbd67c7c172434c1
Roman-PRV <prokopenkorv@gmail.com>
2026-06-29 18:31:19 +0200

    feat: filter game selection list by category from the URL ss-127

--- src/libs/types/game-description-dto.type.ts
+++ src/libs/types/game-description-dto.type.ts
@@ -1,6 +1,8 @@
-import { type GameKey } from "~/libs/enums/enums";
+import { type GameCategory, type GameKey } from "~/libs/enums/enums";
+import { type ValueOf } from "~/libs/types/value-of.type";
 
 type GameDescriptionDto = {
+	categories?: ValueOf<typeof GameCategory>[];
 	description: string;
 	icon_url: string;
 	id: string;
--- src/pages/game-selection-page/game-selection-page.tsx
+++ src/pages/game-selection-page/game-selection-page.tsx
@@ -1,22 +1,34 @@
+import { GameCategory } from "~/libs/enums/enums";
 import {
 	useAppDispatch,
 	useCallback,
 	useEffect,
 	useLanguageSync,
+	useSearchParams,
 	useTranslation,
 } from "~/libs/hooks/hooks";
+import { type ValueOf } from "~/libs/types/types";
 import { actions as gamesActions } from "~/modules/games/games";
 
 import { GameSelectionList } from "./game-selection-list";
 import styles from "./styles.module.css";
 
+const CATEGORY_QUERY_PARAM = "category";
+
+const isGameCategory = (value: null | string): value is ValueOf<typeof GameCategory> =>
+	value !== null && Object.values(GameCategory).includes(value as ValueOf<typeof GameCategory>);
+
 const GameSelectionPage: React.FC = () => {
 	const { t } = useTranslation();
 	const dispatch = useAppDispatch();
+	const [searchParameters] = useSearchParams();
+
+	const categoryParameter = searchParameters.get(CATEGORY_QUERY_PARAM);
+	const category = isGameCategory(categoryParameter) ? categoryParameter : undefined;
 
 	const fetchGames = useCallback(() => {
-		void dispatch(gamesActions.getAllGames());
-	}, [dispatch]);
+		void dispatch(gamesActions.getAllGames(category));
+	}, [dispatch, category]);
 
 	useLanguageSync(fetchGames);
 
